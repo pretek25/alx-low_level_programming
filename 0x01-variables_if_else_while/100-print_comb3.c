6e3871056c0aff839367361bf578be1593e2bbc2
pretek25 <pretek25@gmail.com>
2023-09-10 09:53:24 -0700

    Variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,19 @@
  */
 int main(void)
 {
-	int num1, num2;
-
-	for (num = 0; num1 < 9; num1++)
-	{
-	for (num2 = num1 + 1; num2 < 10; num2++)
-	{
-	putchar((num1 % 10) + '0');
-	putchar((num2 % 10) + '0');
-	if (num == 8 && num2 == 9)
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
-	return (0);
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+if (digit1 == 8 && digit2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
