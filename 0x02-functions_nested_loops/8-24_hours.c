b3de94d0ce511bfa3004ad28b6001236221db153
pretek25 <pretek25@gmail.com>
2023-09-23 15:44:31 -0700

    c-functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
-
 /**
-
  * jack_bauer - prints every minute of the day of Jack Bauer
-
  * starting from 00:00 to 23:59
-
  */
 
 void jack_bauer(void)
-
 {
 
 	int i, j;
@@ -27,7 +22,7 @@ void jack_bauer(void)
 
 	{
 	_putchar((i / 10) + '0');
-	
+
 	_putchar((i % 10) + '0');
 
 	_putchar(':');
@@ -45,5 +40,4 @@ void jack_bauer(void)
 	i++;
 
 	}
-
 }
