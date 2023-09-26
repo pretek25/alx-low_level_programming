80a08ba2f0913615b96c5ba34edfa8c27766686f
pretek25 <pretek25@gmail.com>
2023-09-26 03:20:09 -0700

    c-even

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,13 @@ unsigned int _strspn(char *s, char *accept)
 
 {
 	unsigned int n = 0;
-        int r;
+	int r;
 
 	while (*s)
 	{
 	for (r = 0; accept[r]; r++)
 	{
-	if (*s == accept[r
+	if (*s == accept[r];
 	{
 	n++;
 	break;
